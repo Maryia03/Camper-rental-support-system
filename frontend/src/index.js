fbe8c6e794a0adcaebc30e35b17faecf698a32f5
Maryia03 <164774065+Maryia03@users.noreply.github.com>
2026-02-13 12:03:10 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    {/* Owijamy aplikację w AuthProvider */}
     <AuthProvider>
       <App />
     </AuthProvider>
